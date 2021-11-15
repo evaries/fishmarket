00383820dd9d44a7349a71a27b2bb2d042f8d92f
Evgeniy Nadtochiy <e.nadtochiy@MacBook-Air-Evgeniy.local>
2021-11-15 17:53:45 +0200

    updated url

--- src/api/index.js
+++ src/api/index.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const fetchFish = async () => {
   try {
-    const { data } = await axios.get('http://acnhapi.com/v1/fish/');
+    const { data } = await axios.get('https://acnhapi.com/v1/fish/');
     return data;
   } catch (e) {
     throw e;
